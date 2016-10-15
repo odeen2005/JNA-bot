888adceb6c261d5eee6203a53f92534a0da945d9
NaTsu <odeen2005@hotmail.com>
2016-10-16 01:31:41 +0700

    แก้ไข

--- bot.php
+++ bot.php
@@ -18,9 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'sticker',
-				"packageId": "1",
-   				 "stickerId": "1"
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
